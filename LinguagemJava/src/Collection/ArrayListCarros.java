61dcd6caf4b32740ac037cf93be5a48c2b84d80c
Anderson Freires <55098476+dre36dre@users.noreply.github.com>
2025-03-26 19:57:30 -0300

    Editando arraylistCarros

--- LinguagemJava/src/Collection/ArrayListCarros.java
+++ LinguagemJava/src/Collection/ArrayListCarros.java
@@ -1,6 +1,10 @@
 public class ArrayListCarros{
     public static void main(String[]args){
     
-        ArrayList<>
+        ArrayList<> carros= new ArrayList<>;
+
+
     }
 }
+
+//ver aula 7.c de collectios
