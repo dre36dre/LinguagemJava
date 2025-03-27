8cffde633851fe11c004c94b42b0267ddb2266db
Anderson Freires <55098476+dre36dre@users.noreply.github.com>
2025-03-27 22:27:58 +0000

    Método set de ArrayList

--- LinguagemJava/src/Collections/ArrayaListCarros.java
+++ LinguagemJava/src/Collections/ArrayaListCarros.java
@@ -1,3 +1,20 @@
 public class ArrayListCarros{
-    mai
+    public static void main(String[] args){
+        ArrayList<String> carros= new ArrayList<>();
+
+
+        carros.add("KOMBI");
+        carros.add("FUSCA");
+        carros.add("CARNIVAL");
+        carros.add("SIENA");
+        carros.add("KA");
+
+        System.out.println(carros);
+
+        //substitui FUSCA por FUSION
+
+        carros.set(1,"Fusion");
+
+        
+    }
 }
